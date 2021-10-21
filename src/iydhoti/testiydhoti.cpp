c018397880e902b774c0d871b6616aeb00ca7f7e
Joel Nash <joelnash@xuuki.xyz>
2021-10-22 00:39:33 +0100

    csd file playback

---
+++ src/iydhoti/testiydhoti.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <iostream>
+#include "csound.hpp"
+#include <string>
+
+using namespace std;
+
+
+int main(){
+  Csound* csound = new Csound();
+  csound->CompileCsd("iydhoti.csd");
+  csound->Start();
+  csound->Perform();	
+  delete csound;
+  return 0;
+}
+
