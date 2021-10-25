1de9f02a37d10bb28844964ebda434128009d3b1
Joel Nash <joelnash@xuuki.xyz>
2021-10-25 23:44:07 +0100

    orc/sco file to const char

--- src/iydhoti/testiydhoti.cpp
+++ src/iydhoti/testiydhoti.cpp
@@ -1,13 +1,36 @@
-#include <stdio.h>
 #include <iostream>
-#include "csound.hpp"
+#include <fstream>
+#include <sstream>
 #include <string>
+#include "csound.hpp"
 
 using namespace std;
 
+string readFileIntoString(const string& path) {
+  ifstream input_file(path);
+  if (!input_file.is_open()) {
+    cerr << "Could not open the file - '"
+      << path << "'" << endl;
+   exit(EXIT_FAILURE);
+  }
+  return string((istreambuf_iterator<char>(input_file)), istreambuf_iterator<char>());
+}
+
 int main(){
   Csound* csound = new Csound();
-  csound->CompileCsd("iydhoti.csd");
+
+  string orcfile("o.orc");
+  string orc = readFileIntoString(orcfile);;
+  char * corc = new char [orc.length()+1];
+  strcpy(corc, orc.c_str());
+
+  string scofile("bass_snare.sco");
+  string sco =  readFileIntoString(scofile);
+  char * csco = new char [sco.length()+1];
+  strcpy(csco, sco.c_str());
+
+  csound->CompileOrc(corc);
+  csound->ReadScore(csco);
   csound->Start();
   csound->Perform();	
   delete csound;
