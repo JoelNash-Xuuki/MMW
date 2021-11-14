d983d7809cc9325eb3ef0414745c45ede3183124
Joel Nash <joelnash@xuuki.xyz>
2021-11-14 12:36:59 +0000

    create score pdf

--- src/patchspeclang/main.cpp
+++ src/patchspeclang/main.cpp
@@ -2,10 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "modularsynth.hpp"
+#include "score.hpp"
 
 int main(int argc, char **argv){
   ModularSynth ms= ModularSynth("synth");
   ms.runPatch();
-
+  Score s= Score("test.ly");
   return 0;
 }
